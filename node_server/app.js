6905c19ea731cc3328399b945f92481155167393
Bhanu Teja <bhanuteja5481@gmail.com>
2019-02-07 22:32:51 +0530

    add node_server

---
+++ node_server/app.js
@@ -0,0 +1,57 @@
+const fs = require("fs");
+const express = require("express");
+const Pool = require("pg").Pool;
+
+const app = express();
+const timeStamp = () => new Date();
+
+const pool = new Pool({
+    user: 'postgres',
+    password: '',
+    database: 'postgres',
+    host: 'db',
+    port: 5432
+});
+
+
+
+pool.query("CREATE TABLE IF NOT EXISTS data(numbers int)", (error, results) => {
+    if (error) {
+        console.error("Error -------> " + error);
+        return;
+    }
+    console.log("Table created");
+});
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    let log = `${timeStamp()}: Requested for ${req.url}`;
+
+    fs.appendFile('./logs/requestLogs.log', log + "\n", (err) => {
+        if (err) throw err;
+        console.log(`The "${log}" was appended to file!`);
+    });
+    next();
+});
+
+app.post('/number', (req, res) => {
+    pool.query(`INSERT INTO data VALUES(${req.body.number})`, (error, result) => {
+        if (error) throw error;
+
+        console.log(`Inserted data sucessfully`);
+        res.send("Inserted data sucessfully");
+    });
+});
+
+app.get('/number', (req, res) => {
+    pool.query("SELECT numbers FROM data", (error, results) => {
+        if (error) throw error;
+        let result = results.rows.map(element => element.numbers);
+
+        console.log(result);
+        res.send(JSON.stringify(result));
+    });
+});
+
+module.exports = app;
